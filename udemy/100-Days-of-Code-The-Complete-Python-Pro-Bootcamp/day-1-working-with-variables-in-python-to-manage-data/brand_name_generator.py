234aee24ace016cd3731dd5e1e05a5e55262afe6
nimiyakujjwal <26951554+nimiyakujjwal@users.noreply.github.com>
2024-09-20 09:32:57 +0530

    Day 1 code

---
+++ udemy/100-Days-of-Code-The-Complete-Python-Pro-Bootcamp/day-1-working-with-variables-in-python-to-manage-data/brand_name_generator.py
@@ -0,0 +1,11 @@
+"""
+Brand name generator.
+    Input from user:
+        brand name
+        pet_name
+"""
+
+print("Welcome to Brand Name Generator.")
+brand_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is your pet name\n")
+print("Your brand name could be "+ brand_name + " " + pet_name)
